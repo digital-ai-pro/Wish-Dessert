8377cb5e09fa95adb0dec7bc7b9b28359bd3ad74
Egy Coin <egycoin01@gmail.com>
2025-09-22 06:20:30 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/Wish-Dessert/', // مهم للنشر على GitHub Pages
   server: {
     host: "::",
     port: 8080,
