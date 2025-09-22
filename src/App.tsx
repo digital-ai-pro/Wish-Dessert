0cbe34bbb15c1626527532ed1dbd1e859fe9f12d
Egy Coin <egycoin01@gmail.com>
2025-09-22 06:17:39 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-  <BrowserRouter basename="/WISH-SWEETY">
+  <BrowserRouter basename="/Wish-Dessert">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
